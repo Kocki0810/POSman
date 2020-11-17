af3e03df0194e496fb5435f9874810022068b6d7
Mads <madsi2000@gmail.com>
2020-11-17 12:13:23 +0100

    added storage amount

--- index.js
+++ index.js
@@ -13,9 +13,7 @@ Vue.component('ware-list', {
         wares.wareList[i].id = i;
       }
     }
-}
-  
-})
+}});
 
 Vue.component('ware', {
   props: ['todo'],
@@ -27,27 +25,32 @@ Vue.component('ware', {
 }  
 });
 
+
+
 var wares = new Vue({
   el: '#wares',
   data: {
     newWareName : "",
     newWareDescription: "",
     newWarePrice: "",
+    storageEnabled : false,
+    newStorageAmount : "",
     wareList: []
   },
   methods: {
     saveWare: function () {
       if(this.newWareName != "" && this.newWarePrice != ""){
-        this.wareList.push({id : this.wareList.length, name:this.newWareName, description:this.newWareDescription, price :this.newWarePrice})
+        this.wareList.push({id : this.wareList.length, name : this.newWareName, description : this.newWareDescription, price : this.newWarePrice, storageEnabled : this.storageEnabled, storageAmount : this.newStorageAmount})
         this.newWareName = ""
         this.newWareDescription = ""
         this.newWarePrice = ""
+        this.newStorageAmount = ""
+        this.storageEnabled = false
+
       }
       else{
         alert("Must have a name and a price");
       }
-
-
     }
   }
 });
