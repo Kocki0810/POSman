840138c273dd6b6646f09c13695959e505b8d0c5
Mads <madsi2000@gmail.com>
2020-11-23 14:05:50 +0100

    routing test

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // Routing for home-page
 
 const Foo = { template: '<div>foo</div>' }
@@ -8,9 +9,56 @@ const routes = [
 
 const router = new VueRouter({
     routes
+=======
+const manager = { 
+  template: '<addproductheader v-bind:data="ware"></addproductheader>',
+  methods: {
+    save : function()
+    {
+      document.localStorage.items = JSON.stringify(this.wareList);
+    },
+    load : function()
+    {
+      var json_string = document.localStorage["items"];
+      if(json_string == undefined)
+      {
+        this.wareList = [];
+      }
+      else
+      {
+        this.wareList = JSON.parse(json_string);
+      }
+    }
+  },
+  data(){
+    return {wareList : []};
+  },
+  mounted(){
+    this.load();
+  }
+}
+const cashier = { 
+template: '<div>bar</div>', 
+
+methods : {
+  mounted(){
+    this.load();
+  }
+ }
+}
+
+const routes = [
+    { path: '/#', component: manager },
+    { path: '/#cashier', component: cashier }
+];
+
+const router = new VueRouter({
+routes 
+>>>>>>> Stashed changes
 })
 
 var wares = new Vue({
+  router,
   el: '#wares',
   data: {
     ware: {
@@ -22,10 +70,15 @@ var wares = new Vue({
     wareList : [],
     waresSold : []
     }
+<<<<<<< Updated upstream
   },
   methods: {
  
     },
   router
 }).$mount('#wares');
+=======
+  }
+});
+>>>>>>> Stashed changes
 
