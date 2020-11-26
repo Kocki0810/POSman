b1f41eb83297590bc2e88cb95cff054df3cc52ba
Cliff <clif0024@elev.techcollege.dk>
2020-11-26 10:28:31 +0100

    Added total price

--- components/cart.js
+++ components/cart.js
@@ -2,6 +2,15 @@ Vue.component('cart', {
     name: "cart",
     props: ['cartitems'],
     template: document.getElementById("cartTemplate"),
+    computed: {
+        total: function () {
+            let sum = 0;
+            this.cartitems.forEach(function (item) {
+                sum += parseFloat(item.price);
+            });
+            return sum;
+        }
+    },
     methods: {
         clickadd: function (item) {
             this.$emit('addtocart', item)
